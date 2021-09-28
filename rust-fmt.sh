5a166967c61bf71778ec07d60accd120de215bd6
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2021-09-28 14:32:47 -0700

    rust-fmt.sh 120

--- rust-fmt.sh
+++ rust-fmt.sh
@@ -4,7 +4,8 @@ set -x
 
 exec rustfmt \
    -v \
-   --config fn_call_width=100 \
+   --config fn_call_width=120 \
+   --config fn_args_layout=Compressed \
    --config max_width=120 \
    --config newline_style=unix \
    --config edition=2021 \
