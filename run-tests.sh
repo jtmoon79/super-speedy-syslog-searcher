5ce130bf1ceb3ef5067a1e36b7ca39090083d8c1
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2022-03-19 01:58:54 -0700

    run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -49,13 +49,18 @@ declare -a files=(
     "${rootd}/logs/other/tests/test2.log"
     "${rootd}/logs/other/tests/test3-hex.log"
     "${rootd}/logs/other/tests/basic-dt.log"
-    "${rootd}/logs/Ubuntu18/samba/log.10.7.220.19"  # multiline syslines
+    "${rootd}/logs/other/tests/basic-basic-dt20.log"
+    "${rootd}/logs/Ubuntu18/samba/log.10.7.220.19"  # multi-line syslines
     "${rootd}/logs/debian9/syslog"  # very large file!
 )
 
 for file_ in "${files[@]}"; do
     for sz in 1 2 3 4 5 6 8 10 12 14 16 18 19 20 21 22 32 64 128 1024 2056 4096 8192 16284 32568 65536 131702
     do
+        if [[ ! -r "${file_}" ]]; then
+            echo -e "\e[33mWarning: skip file not readable or not exist '${file_}'\e[39m" >&2
+            continue
+        fi
         declare -i fsz=
         fsz=$(filesz "${file_}")
         if [[ ${fsz} -gt 100000 ]] && [[ ${sz} -lt 64 ]]; then
