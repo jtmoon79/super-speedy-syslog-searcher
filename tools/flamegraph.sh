71b4224b7c9048b12e91a7ece0ea2292f64d0f41
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2022-03-31 13:25:58 -0700

    add gen-*.log files

--- tools/flamegraph.sh
+++ tools/flamegraph.sh
@@ -2,52 +2,54 @@
 #
 # install:
 #   apt install -y linux-perf linux-tools-generic
+#   cargo install flamegraph
 #
-# add to Cargo.toml
-#   [profile.bench]
-#   debug = true
-#   [profile.release]
-#   debug = true
 
 set -euo pipefail
 
 cd "$(dirname -- "${0}")/.."
 
 function did_install () {
-    echo "Did you install:" >&2
-    echo "    linux-perf" >&2
-    echo "    linux-tools-generic" >&2 
+    echo "Did you:" >&2
+    echo "    apt install -y linux-perf linux-tools-generic" >&2
+    echo "    cargo install flamegraph" >&2
 }
 
-export CARGO_PROFILE_RELEASE_DEBUG=true
-PERF="/usr/lib/linux-tools/$(ls -1v /usr/lib/linux-tools/ | tail -n1)/perf"
+PERF=${PERF-"/usr/lib/linux-tools/$(ls -1v /usr/lib/linux-tools/ | tail -n1)/perf"}
 if [[ ! -e "${PERF}" ]]; then
     echo "PERF tool does not exist '${PERF}'" >&2
     did_install
     exit 1
 fi
 export PERF
+
 if ! which flamegraph; then
     echo "flamegraph is not in the PATH" >&2
     did_install
     exit 1
 fi
 
-echo "Cargo.toml must have:
+#echo "Cargo.toml must have:
+#
+#    [profile.bench]
+#    debug = true
+#    [profile.release]
+#    debug = true
+#
+#" >&2
 
-    [profile.bench]
-    debug = true
-    [profile.release]
-    debug = true
-
-" >&2
-
+export CARGO_PROFILE_RELEASE_DEBUG=true
 export RUST_BACKTRACE=1
 (
     set -x
-    cargo build --release
-    flamegraph -o flamegraph.svg \
-      ./target/release/super_speedy_syslog_searcher \
-      --path $(find ./logs/other/tests/ -type f -not \( -name '*.gz' -o -name '*.xz' -o -name '*.tar' -o -name '*.zip' \) ) \
-      -- 0xFFFFF '20000101T000100' >/dev/null
+    cargo flamegraph --version
+    cargo flamegraph -vv --deterministic -o 'flamegraph.svg' -- \
+        ./target/release/super_speedy_syslog_searcher \
+          -z 0xFFFFF -a '20000101T000100' \
+           $(find ./logs/other/tests/ -type f -not \( -name '*.gz' -o -name '*.xz' -o -name '*.tar' -o -name '*.zip' \) ) \
+           >/dev/null
+    #flamegraph -o flamegraph.svg \
+    #  ./target/release/super_speedy_syslog_searcher \
+    #  --path $(find ./logs/other/tests/ -type f -not \( -name '*.gz' -o -name '*.xz' -o -name '*.tar' -o -name '*.zip' \) ) \
+    #  -- 0xFFFFF '20000101T000100' >/dev/null
 )
