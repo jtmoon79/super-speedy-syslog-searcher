64c9c1a31ec4360034f3cad722e01d29cee4dc15
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2022-04-02 16:39:54 -0700

    bench_ranges.rs incomplete

---
+++ benches/bench_ranges.rs
@@ -0,0 +1,33 @@
+// bench_ranges.rs
+//
+// using `RangeMap` is surprisingly expensive, according to `tools/flamegraph.sh`
+// https://docs.rs/rangemap/latest/rangemap/
+// 
+
+extern crate criterion;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+extern crate rangemap;
+use rangemap::{RangeMap,RangeSet};
+
+type RangeMapT = RangeMap<u64, u64>;
+type RangeSetT = RangeSet<u64>;
+
+fn baseline_no_ranges() {
+    return;
+}
+
+// TODO: compare `RangeMapT` and `RangeSetT` times for `insert` and `get` and `contains`
+//       also compare `BTreeMap.contains_key`
+
+// criterion runners
+
+fn criterion_benchmark(c: &mut Criterion) {
+    
+    let mut bg = c.benchmark_group("RangeMap");
+    bg.bench_function("baseline_no_ranges", |b| b.iter(|| baseline_no_ranges()));
+
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
