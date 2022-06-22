c9119a7381e804791a630c3e8e2cada0592d7316
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2022-06-21 22:04:27 -0700

    valgrind-callgrind add CLI options
    valgrind_callgrind collects more data, `bus` and `systime`
    
    callgrind_annotate prints `both` types of output

--- tools/valgrind-callgrind.sh
+++ tools/valgrind-callgrind.sh
@@ -18,6 +18,12 @@ if ! valgrind=$(which valgrind); then
     echo "    sudo apt install valgrind g++" >&2
     exit 1
 fi
+if ! callgrind=$(which callgrind_annotate); then
+    echo "callgrind not found in PATH" >&2
+    echo "install:" >&2
+    echo "    sudo apt install valgrind" >&2
+    exit 1
+fi
 
 declare -r bin=./target/release/s4
 
@@ -25,6 +31,7 @@ declare -r bin=./target/release/s4
 (set -x; git log -n1 --format='%h %D')
 (set -x; "${bin}" --version)
 (set -x; $valgrind --version) | head -n1
+(set -x; $callgrind --version) || true  # --version causes process return code 255
 
 echo
 
@@ -34,17 +41,25 @@ declare -a files=(
 
 OUT=./callgrind.out
 rm -f "${OUT}"
-(
+
 #export RUST_BACKTRACE=1
-set -x;
-    valgrind --tool=callgrind --callgrind-out-file="${OUT}" \
+set -x
+
+$valgrind --tool=callgrind \
+    --collect-bus=yes \
+    --collect-systime=yes \
+    `#--separate-threads=yes` \
+    --callgrind-out-file="${OUT}" \
     -- \
     "${bin}" \
-    -z 0xFFFF \
-    -a 20000101T000000 -b 20000101T080000 \
-    "${files[@]}" \
+        -z 0xFFFF \
+        -a 20000101T000000 -b 20000101T080000 \
+        "${files[@]}" \
     >/dev/null
-)
 
-callgrind_annotate --tree=both --show-percs=yes "${OUT}"
-
+exec \
+    $callgrind \
+    --tree=both \
+    --show-percs=yes \
+    $(find ./src -xdev -type d -exec echo -n '--include={} ' \;) \
+    "${OUT}"
