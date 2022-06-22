5d5834e96c038c347a91adbf5047f5ab3b10f60b
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2022-06-21 22:03:21 -0700

    add backup-dirty.sh for quick and dirty local backups
    before making drastic changes, quickly set aside current source
    files into a dated zip file.

---
+++ tools/backup-dirty.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+# backup-dirty.sh
+#
+# a quick and dirty backup script using 7zip
+#
+
+set -euo pipefail
+
+cd "$(dirname "${0}")/.."
+
+HERE="$(basename -- "$(realpath .)")"
+ZIPFILE="../${HERE}-$(date '+%Y%m%dT%H%M%S').zip"
+
+Zz=$(which 7z)
+
+set -x
+
+"${Zz}" a -bb1 -bt -stl -snl -tzip "${ZIPFILE}" \
+    ./Cargo.toml \
+    ./Cargo.lock \
+    ./benches \
+    ./.github \
+    ./.gitignore \
+    ./LICENSE.txt \
+    ./logs \
+    ./performance-data \
+    ./README.md \
+    ./src \
+    ./tools \
+
+"${Zz}" l "${ZIPFILE}"
+
+echo -e "\n\n\n"
+
+ls -lh "${ZIPFILE}"
