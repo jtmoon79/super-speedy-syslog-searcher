378c0c0e0c1c1478f039b34e9696530702cc5ea5
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2022-08-13 23:16:07 -0700

    syslogprocessor_tests.rs add more tests

--- src/tests/syslogprocessor_tests.rs
+++ src/tests/syslogprocessor_tests.rs
@@ -6,11 +6,14 @@
 #![allow(non_camel_case_types)]
 
 use crate::common::{
+    FileOffset,
     FPath,
 };
 
 use crate::printer_debug::helpers::{
+    NamedTempFile,
     create_temp_file,
+    create_temp_file_bytes,
     ntf_fpath,
 };
 
@@ -23,12 +26,146 @@ use crate::readers::filepreprocessor::{
 };
 
 use crate::data::datetime::{
+    DateTimeL,
     FixedOffset,
+    SystemTime,
+    Year,
+    DateTimePattern_str,
+    datetime_parse_from_str,
+};
+
+use crate::readers::syslinereader::{
+    ResultS4SyslineFind,
 };
 
 use crate::readers::syslogprocessor::{
     SyslogProcessor,
+    FileProcessingResultBlockZero,
 };
+
+extern crate const_format;
+use const_format::concatcp;
+
+extern crate lazy_static;
+use lazy_static::lazy_static;
+
+extern crate test_case;
+use test_case::test_case;
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+// -------------------------------------------------------------------------------------------------
+
+// TODO: [2022/06/01] these are repeated in several `_test.rs` files, declare them in
+//       one common `common_tests.rs` file
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+const SZ: BlockSz = SyslogProcessor::BLOCKSZ_MIN;
+
+const NTF5_DATA_LINE0: &str = "Jan 1 01:00:00 5a\n";
+const NTF5_DATA_LINE1: &str = "Feb 29 02:00:00 5b\n";
+const NTF5_DATA_LINE2: &str = "Mar 3 03:00:00 5c\n";
+const NTF5_DATA_LINE3: &str = "Apr 4 04:00:00 5d\n";
+const NTF5_DATA_LINE4: &str = "May 5 05:00:00 5e\n";
+
+const NTF5_DATA: &str = concatcp!(
+    NTF5_DATA_LINE0,
+    NTF5_DATA_LINE1,
+    NTF5_DATA_LINE2,
+    NTF5_DATA_LINE3,
+    NTF5_DATA_LINE4,
+);
+
+const NTF5_DATA_LINE0_OFFSET: usize = 0;
+const NTF5_DATA_LINE1_OFFSET: usize = NTF5_DATA_LINE0.as_bytes().len();
+const NTF5_DATA_LINE2_OFFSET: usize = NTF5_DATA_LINE1_OFFSET + NTF5_DATA_LINE1.as_bytes().len();
+const NTF5_DATA_LINE3_OFFSET: usize = NTF5_DATA_LINE2_OFFSET + NTF5_DATA_LINE2.as_bytes().len();
+const NTF5_DATA_LINE4_OFFSET: usize = NTF5_DATA_LINE3_OFFSET + NTF5_DATA_LINE3.as_bytes().len();
+
+const NTF5_LINE2_DATETIME_STR: &str = "Mar 3 03:00:00 +0000";
+const NTF5_LINE2_DATETIME_PATTERN: &DateTimePattern_str = "%b %e %H:%M:%S %z";
+
+
+const NTF3_DATA_LINE0: &str = "Jan 1 01:00:00 2000 A3\n";
+const NTF3_DATA_LINE1: &str = "Feb 2 02:00:00 2000 B3\n";
+const NTF3_DATA_LINE2: &str = "Mar 3 03:00:00 2000 C3\n";
+
+const NTF3_DATA: &str = concatcp!(
+    NTF3_DATA_LINE0,
+    NTF3_DATA_LINE1,
+    NTF3_DATA_LINE2,
+);
+
+const NTF3_LINE1_DATETIME_STR: &str = "Feb 2 02:00:00 2000 +0000";
+const NTF3_LINE1_DATETIME_PATTERN: &DateTimePattern_str = "%b %e %H:%M:%S %Y %z";
+
+lazy_static! {
+    static ref TIMEZONE_0: FixedOffset = FixedOffset::west(0);
+
+    // NTF5
+
+    static ref NTF5_LINE2_DATETIME: DateTimeL = {
+        match datetime_parse_from_str(
+            NTF5_LINE2_DATETIME_STR, NTF5_LINE2_DATETIME_PATTERN, true, &TIMEZONE_0
+        ) {
+            Some(dt) => dt,
+            None => {
+                panic!("bad parameters to datetime_parse_from_str for NTF5_LINE2_DATETIME_STR");
+            }
+        }
+    };
+
+    static ref NTF5: NamedTempFile = {
+        create_temp_file(&NTF5_DATA)
+    };
+
+    static ref NTF5_PATH: FPath = {
+        ntf_fpath(&NTF5)
+    };
+
+    // NTF3
+
+    static ref NTF3_LINE1_DATETIME: DateTimeL = {
+        match datetime_parse_from_str(
+            NTF3_LINE1_DATETIME_STR, NTF3_LINE1_DATETIME_PATTERN, true, &TIMEZONE_0
+        ) {
+            Some(dt) => dt,
+            None => {
+                panic!("bad parameters to datetime_parse_from_str for NTF3_LINE1_DATETIME_STR");
+            }
+        }
+    };
+
+    static ref NTF3: NamedTempFile = {
+        create_temp_file(&NTF3_DATA)
+    };
+
+    static ref NTF3_PATH: FPath = {
+        ntf_fpath(&NTF3)
+    };
+
+    // 76208400
+    // Thursday, June 1, 1972 1:00:00 AM GMT+00:00
+    // Wednesday, May 31, 1972 6:00:00 PM GMT-07:00
+    static ref SYSTEMTIME_1972_06_01: SystemTime = {
+        let duration: std::time::Duration = std::time::Duration::from_secs(76208400);
+
+        SystemTime::UNIX_EPOCH.checked_add(duration).unwrap()
+    };
+    // 107744400
+    // Friday, June 1, 1973 1:00:00 AM GMT+00:00
+    // Thursday, May 31, 1973 6:00:00 PM GMT-07:00
+    static ref SYSTEMTIME_1973_06_01: SystemTime = {
+        let duration: std::time::Duration = std::time::Duration::from_secs(107744400);
+
+        SystemTime::UNIX_EPOCH.checked_add(duration).unwrap()
+    };
+}
+
+const YEAR_1972: Year = 1972;
+const YEAR_2019: Year = 2019;
+const YEAR_2044: Year = 2044;
 
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
@@ -46,13 +183,6 @@ fn new_SyslogProcessor(path: &FPath, blocksz: BlockSz) -> SyslogProcessor {
 
 // -------------------------------------------------------------------------------------------------
 
-// TODO: [2022/06/01] these are repeated in several `_test.rs` files, declare them in
-//       one common `common_tests.rs` file
-
-// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-const SZ: BlockSz = SyslogProcessor::BLOCKSZ_MIN;
-
 /// test `SyslogProcessor::new`
 #[test]
 fn test_SyslogProcessor_new1() {
@@ -60,6 +190,144 @@ fn test_SyslogProcessor_new1() {
     let path = ntf_fpath(&ntf);
     let slp = new_SyslogProcessor(&path, SZ);
     eprintln!("{:?}", slp);
+}
+
+// -------------------------------------------------------------------------------------------------
+
+#[test]
+fn test_process_missing_year_1972() {
+    let mut slp = new_SyslogProcessor(&NTF5_PATH, SZ);
+    slp.process_missing_year(SYSTEMTIME_1972_06_01.clone());
+}
+
+// -------------------------------------------------------------------------------------------------
+
+#[test]
+fn test_find_sysline() {
+    let mut slp = new_SyslogProcessor(&NTF5_PATH, SZ);
+    let mut fo: FileOffset = 0;
+    loop {
+        let result = slp.find_sysline(fo);
+        match result {
+            ResultS4SyslineFind::Found((fo_, syslinep)) => {
+                fo = fo_;
+            }
+            ResultS4SyslineFind::Done => {
+                break;
+            }
+            ResultS4SyslineFind::Err(err) => {
+                panic!("Error {:?}", err);
+            }
+        }
+    }
+}
+
+#[test]
+fn test_find_sysline_between_datetime_filters_Found() {
+    let mut slp = new_SyslogProcessor(&NTF5_PATH, SZ);
+    let mut fo: FileOffset = 0;
+
+    let result = slp.find_sysline_between_datetime_filters(fo);
+    match result {
+        ResultS4SyslineFind::Found((_fo, _syslinep)) => {}
+        ResultS4SyslineFind::Done => {
+            panic!("Unexpected Done");
+        }
+        ResultS4SyslineFind::Err(err) => {
+            panic!("Error {:?}", err);
+        }
+    }
+}
+
+#[test]
+fn test_find_sysline_between_datetime_filters_Done() {
+    let mut slp = new_SyslogProcessor(&NTF5_PATH, SZ);
+    let fo: FileOffset = NTF5_DATA.len() as FileOffset;
+
+    let result = slp.find_sysline_between_datetime_filters(fo);
+    match result {
+        ResultS4SyslineFind::Found((_fo, _syslinep)) => {
+            panic!("Unexpected Found");
+        }
+        ResultS4SyslineFind::Done => {}
+        ResultS4SyslineFind::Err(err) => {
+            panic!("Error {:?}", err);
+        }
+    }
+}
+
+// -------------------------------------------------------------------------------------------------
+
+#[test_case(0x400)]
+#[test_case(0x10 => panics)]  // Issue #22
+fn test_processing_stage_1_blockzero_analysis(blocksz: BlockSz) {
+    let mut slp = new_SyslogProcessor(&NTF3_PATH, blocksz);
+
+    match slp.process_stage0_valid_file_check() {
+        FileProcessingResultBlockZero::FileOk => {}
+        result => {
+            panic!("Unexpected result stage0 {:?}", result);
+        }
+    }
+
+    match slp.process_stage1_blockzero_analysis() {
+        FileProcessingResultBlockZero::FileOk => {}
+        result => {
+            panic!("Unexpected result stage1 {:?}", result);
+        }
+    }
+}
+
+#[test]
+fn test_processing_stages_0_5() {
+    let mut slp = new_SyslogProcessor(&NTF3_PATH, 0x400);
+
+    match slp.process_stage0_valid_file_check() {
+        FileProcessingResultBlockZero::FileOk => {}
+        result => {
+            panic!("Unexpected result stage0 {:?}", result);
+        }
+    }
+
+    match slp.process_stage1_blockzero_analysis() {
+        FileProcessingResultBlockZero::FileOk => {}
+        result => {
+            panic!("Unexpected result stage1 {:?}", result);
+        }
+    }
+
+    match slp.process_stage2_find_dt() {
+        FileProcessingResultBlockZero::FileOk => {}
+        result => {
+            panic!("Unexpected result stage2 {:?}", result);
+        }
+    }
+
+    match slp.process_stage3_stream_syslines() {
+        FileProcessingResultBlockZero::FileOk => {}
+        result => {
+            panic!("Unexpected result stage3 {:?}", result);
+        }
+    }
+
+    let mut fo: FileOffset = 0;
+    loop {
+        let result = slp.find_sysline(fo);
+        match result {
+            ResultS4SyslineFind::Found((fo_, syslinep)) => {
+                fo = fo_;
+            }
+            ResultS4SyslineFind::Done => {
+                break;
+            }
+            ResultS4SyslineFind::Err(err) => {
+                panic!("Error {:?}", err);
+            }
+        }
+    }
+
+    let _summary = slp.process_stage4_summary();
+
 }
 
 // -------------------------------------------------------------------------------------------------
