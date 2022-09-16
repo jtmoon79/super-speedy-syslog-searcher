97cf45e94786b87b5a2d3fb2ecf2e696aeb4d1d9
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2022-09-15 22:10:50 -0700

    add cargo-test-cov-llvm.sh

---
+++ tools/cargo-test-cov-llvm.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+#
+# run `cargo test` with code coverage report with llvm
+#
+# instructions from https://doc.rust-lang.org/rustc/instrument-coverage.html
+
+set -eu
+
+cd "$(dirname -- "${0}")/.."
+
+if ! which llvm-profdata &>/dev/null; then
+    echo "Cannot find llvm-profdata in PATH" >&2
+    echo "Is 'llvm' installed? Try:" >&2
+    echo "    apt install llvm" >&2
+    exit 1
+fi
+if ! which jq &>/dev/null; then
+    echo "Cannot find jq in PATH" >&2
+    exit 1
+fi
+
+cov_file="instrument-coverage-llvm.profraw.json"
+pdata_file="instrument-coverage-llvm.profdata"
+
+rm -vf -- "${cov_file}" "${pdata_file}"
+
+export RUSTFLAGS=${RUSTFLAGS-"-C instrument-coverage"}
+
+function list_objects () {
+    declare objectf=
+    for objectf in $(
+            cargo test --tests --no-run --message-format=json \
+            | jq -r 'select(.profile.test == true) | .filenames[]' \
+            | grep -v dSYM -
+    ); do
+        echo -n '--object' "${objectf}" ' '
+    done
+}
+
+demangler=''
+if cargo install --list | grep -q '^rustfilt '; then
+    demangler='--Xdemangler=rustfilt'
+fi
+
+echo "RUSTFLAGS '${RUSTFLAGS}'"
+
+set -x
+
+cargo clean
+
+LLVM_PROFILE_FILE="${cov_file}" \
+    cargo test --tests
+
+llvm-profdata merge -sparse "${cov_file}" -o "${pdata_file}"
+
+llvm-cov report \
+    --use-color --ignore-filename-regex='/.cargo/registry' \
+    --instr-profile="${pdata_file}" \
+    $(list_objects)
+
+#llvm-cov show \
+#    --use-color --ignore-filename-regex='/.cargo/registry' \
+#    --instr-profile="${pdata_file}" \
+#    --show-instantiations --show-line-counts-or-regions \
+#    "${demangler}" \
+#    $(list_objects)
