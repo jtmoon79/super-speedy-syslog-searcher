6d64fd6d8ee1b5338877004d22ecfaf18ed47ba7
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2022-09-17 00:56:16 -0700

    valgrind-dhat.sh add more logs to parse

--- tools/valgrind-dhat.sh
+++ tools/valgrind-dhat.sh
@@ -32,11 +32,28 @@ echo
 
 declare -a files=(
     $(ls -1 \
+        ./logs/other/tests/dtf5-3-LF-CR.log \
+        ./logs/other/tests/dtf5-6a.log.gz.tar \
+        ./logs/other/tests/dtf5-6a.log.xz \
+        ./logs/other/tests/dtf7-20-LEVELS.log \
+        ./logs/other/tests/dtf7-20-LEVELS.log \
+        ./logs/other/tests/dtf7-20-LEVELS.log.gz \
+        ./logs/other/tests/dtf7-20-LEVELS.log.old \
+        ./logs/other/tests/dtf7-20-LEVELS.log.old.gz \
+        ./logs/other/tests/dtf7-20-LEVELS.log.tar \
+        ./logs/other/tests/dtf7-20-LEVELS.log.xz \
+        ./logs/other/tests/dtf9d-12x3-37.log \
+        ./logs/other/tests/gen-20-1-🌚🌛🌜🌝.log \
+        ./logs/other/tests/gen-20-1-⚀⚁⚂⚃⚄⚅.log \
         ./logs/other/tests/gen-100-10-.......log \
         ./logs/other/tests/gen-100-10-BRAAAP.log \
         ./logs/other/tests/gen-100-10-FOOBAR.log \
         ./logs/other/tests/gen-100-10-______.log \
         ./logs/other/tests/gen-100-10-skullcrossbones.log \
+        ./logs/other/tests/gen-100-10-skullcrossbones.log.gz \
+        ./logs/other/tests/gen-100-10-skullcrossbones.log.xz \
+        ./logs/other/tests/gen-100-10-skullcrossbones.tar \
+        ./logs/other/tests/gen-100-10.tar \
         ./logs/other/tests/gen-100-4-happyface.log \
         ./logs/other/tests/gen-1000-3-foobar.log \
         ./logs/other/tests/gen-200-1-jajaja.log \
