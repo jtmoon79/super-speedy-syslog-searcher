bfb8f12d4084e2f969f59b260b968d9c1154f002
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2022-10-10 14:36:57 -0700

    changelog-link-gen.sh link Issues
    [ci skip]

--- tools/changelog-link-gen.sh
+++ tools/changelog-link-gen.sh
@@ -15,18 +15,33 @@ cd "$(dirname -- "${0}")/.."
 readonly URL_PROJECT='https://github.com/jtmoon79/super-speedy-syslog-searcher'
 CHANGELOG='./CHANGELOG.md'
 
+# match Issue link
+#
+#    [Issue #3]
+#
+# prints
+#
+#    [Issue #3]: https://github.com/jtmoon79/super-speedy-syslog-searcher/issues/3
+#
+grep -oEe '\[Issue #([[:digit:]]+)\]' -- "${CHANGELOG}" \
+    | tr -d '[]' \
+    | sort -n -t '#' -k2 \
+    | sed -Ee 's|^Issue #([[:digit:]]+)$|[Issue #\1]: '"${URL_PROJECT}"'/issues/\1|g' \
+    | uniq
+
 # match tag comparison link, e.g.
 #
 #    [0.0.25...0.0.26]
 #
 # prints
 #
-#     [0.0.25...0.0.26]: https://github.com/jtmoon79/super-speedy-syslog-searcher/compare/0.0.25...0.0.26
+#    [0.0.25...0.0.26]: https://github.com/jtmoon79/super-speedy-syslog-searcher/compare/0.0.25...0.0.26
 #
 grep -oEe '\[[[:digit:]]{1,2}\.[[:digit:]]{1,2}\.[[:digit:]]{1,2}\.\.\.[[:digit:]]{1,2}\.[[:digit:]]{1,2}\.[[:digit:]]{1,2}\]' -- "${CHANGELOG}" \
     | tr -d '[]' \
+    | sort -n -t '.' -k1 -k2 -k3 \
     | sed -Ee 's|^(.+)$|[\1]: '"${URL_PROJECT}"'/compare/\1|g' \
-    | sort | uniq
+    | uniq
 
 # match full git hash link, e.g.
 #
