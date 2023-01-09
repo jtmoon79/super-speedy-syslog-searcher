3ac5374edd67a53e0c1492e487db90e9d36a91fd
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2023-01-09 01:46:05 -0800

    rust-workflow.sh run compare scripts
    Also run `compare-current-and-expected.sh` and
    `compare-debug-release.sh`.

--- tools/rust-workflow.sh
+++ tools/rust-workflow.sh
@@ -36,4 +36,6 @@ cargo doc --locked --release --frozen --no-deps
 cargo publish --dry-run --allow-dirty
 "${S4R}" --color=never "${S4_TEST_FILES[@]}" 2>/dev/null
 "${S4D}" --color=never "${S4_TEST_FILES[@]}" 2>/dev/null
+./tools/compare-current-and-expected.sh
+./tools/compare-debug-release.sh
 ./tools/compare-grep-sort.sh
