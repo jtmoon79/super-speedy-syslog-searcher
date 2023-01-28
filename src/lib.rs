1373cfcc4c56c129e64a2b4f85ed224082c6ebd0
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2023-01-27 20:04:53 -0800

    lib.rs tweak opening doc sentence

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,7 @@
 //! _Super Speedy Syslog Searcher_ library, _s4lib_!
 //!
 //! This is the library implementation used by binary program _s4_.
-//! This library is documented in part to have a presence on _crates.io_ and
-//! _docs.rs_.
+//! This library is documented in part to have a presence on _docs.rs_.
 //!
 //! The _s4lib_ library was not designed for use outside of program _s4_,
 //! and it was not designed to be an especially user-friendly API.
