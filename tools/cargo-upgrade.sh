cb698ec9a74329fc7947db489769472f0cbaf4e8
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2023-03-18 01:43:45 -0700

    add tools/cargo-upgrade.sh

---
+++ tools/cargo-upgrade.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# run `cargo upgrade` with preferred options
+#
+# to install:
+#     cargo install cargo-edit
+
+set -eu
+
+cd "$(dirname -- "${0}")/.."
+
+if ! cargo upgrade --version; then
+    echo "Is cargo upgrade installed?" >&2
+    echo "    cargo install cargo-edit" >&2
+    exit 1
+fi
+
+(
+set -x
+cargo upgrade --verbose "${@}"
+)
+
+echo >&2
+echo "To update Cargo.lock, run:" >&2
+echo "    cargo update --verbose" >&2
