38a5a1297b0641b04abea6522698b1e29ae46efd
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2023-04-18 23:13:19 -0700

    add tools/rsync-project-to.sh
    For quick rsyncs of the project.

---
+++ tools/rsync-project-to.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+#
+# rsync this project to another directory.
+# only copy relevant project files, not `target` directory, `.git` directory,
+# and other temporary files.
+#
+# On occasion, I want to quickly rsync this project to another directory,
+# and then run some `cargo` command in that directory.
+#
+
+set -eu
+
+if [[ ${#} != 1 ]]; then
+    echo "usage: ${0} <target-directory>" >&2
+    exit 1
+fi
+
+cd "$(dirname "${0}")/.."
+HERE="$(basename -- "$(realpath .)")"
+RSYNC=$(which rsync)
+TARGET=${1}
+
+if [[ ! -e "${TARGET}" ]]; then
+    mkdir -vp -- "${TARGET}"
+fi
+if [[ ! -d "${TARGET}" ]]; then
+    echo "ERROR: ${TARGET} exists and is not a directory" >&2
+    exit 1
+fi
+if [[ -n "$(ls -A "${TARGET}/" 2>/dev/null)" ]]; then
+    echo "ERROR: directory ${TARGET} is not empty" >&2
+    exit 1
+fi
+
+set -x
+exec \
+"${RSYNC}" \
+    --verbose \
+    --recursive \
+    --archive \
+    --copy-links \
+    --exclude '/bindgen/target/' \
+    ./benches \
+    ./bindgen \
+    ./Cargo.toml \
+    ./Cargo.lock \
+    ./CHANGELOG.md \
+    ./Extended-Thoughts.md \
+    ./.github \
+    ./LICENSE.txt \
+    ./logs \
+    $(ls -d1 \
+        ./valgrind \
+        ./Notes.txt \
+        ./flamegraph.svg* \
+        ./tests \
+        2>/dev/null || true
+    ) \
+    ./README.md \
+    ./rustfmt.toml \
+    ./src \
+    ./tools \
+    "${TARGET}" \
