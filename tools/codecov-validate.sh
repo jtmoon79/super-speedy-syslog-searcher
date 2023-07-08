a4eabe11f15c788abeabbad8d11a447a99d3414c
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2023-07-08 12:57:37 -0700

    rust.yml add job codecov.yml validation

--- tools/codecov-validate.sh
+++ tools/codecov-validate.sh
@@ -5,8 +5,21 @@
 
 set -eu
 
+HOST_VAL="codecov.io"
+URL_VAL="https://${HOST_VAL}/validate"
+
 cd "$(dirname -- "${0}")/.."
+
+if which dig &>/dev/null; then
+    dig +short "${HOST_VAL}" &>/dev/null || {
+        echo "ERROR: cannot DNS resolve '${HOST_VAL}'; skip YAML validation and exit 0" >&2
+        exit 0
+    }
+fi
+
+(set -x; curl --version)
+echo
 
 set -x
 
-exec curl -v -X POST --data-binary @./.github/codecov.yml "https://codecov.io/validate"
+exec curl -X POST --data-binary "@./.github/codecov.yml" "${URL_VAL}" "${@}"
