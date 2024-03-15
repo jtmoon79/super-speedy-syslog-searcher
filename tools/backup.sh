28939470bb91be060c3555fefd993deea582a3c6
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2024-03-15 16:50:58 -0700

    backup.sh allow exported BACKUPDIR

--- tools/backup.sh
+++ tools/backup.sh
@@ -4,25 +4,45 @@
 #
 # a quick manual backup script using 7zip
 #
+# set BACKUPDIR to the final backup destination, defaults to ".."
+#
 
 set -euo pipefail
 
 cd "$(dirname "${0}")/.."
 
-HERE="$(basename -- "$(realpath .)")"
-ZIPFILE="../${HERE}-$(date '+%Y%m%dT%H%M%S')-$(hostname).zip"
+if [[ ! -d "${BACKUPDIR-.}" ]]; then
+    echo "BACKUPDIR is not a directory or does not exist '${BACKUPDIR-}'" >&2
+    exit 1
+fi
+
+BACKUPDIR=${BACKUPDIR:-".."}
+
+if [[ ! -w "${BACKUPDIR}" ]]; then
+    echo "BACKUPDIR is not writable '${BACKUPDIR}'" >&2
+    exit 1
+fi
 
 Zz=$(which 7z)
 
+# limit archived log files to 30M or less
 declare -a logs=()
 while read log; do
     logs[${#logs[@]}]=${log}
-done <<< $(find ./logs -xdev -type f -size -20M | sort)
+done <<< $(find ./logs -xdev -type f -size -30M | sort)
+
+HERE="$(basename -- "$(realpath .)")"
+ZIPFILE="${BACKUPDIR}/${HERE}-$(date '+%Y%m%dT%H%M%S')-$(hostname).zip"
 
 (
 set -x
 
-"${Zz}" a -spf -bb1 -bt -stl -snl -tzip -- "${ZIPFILE}" \
+# if this fails then `7z` is probably not the 7-zip this script expects
+"${Zz}" i &> /dev/null
+
+# backup the project!
+# ignore `target` directories in bindgen/ and the root
+"${Zz}" a -spf -ssc -bb1 -bt -stl -snl -tzip -- "${ZIPFILE}" \
     ./benches/ \
     $(find ./bindgen/ \
         -mindepth 1 -maxdepth 1 \
