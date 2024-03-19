a0ec951e4ea460e2b98afeac36f5d7d38767c4a1
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2024-03-18 22:10:32 -0700

    yamllint.sh allow python or python3

--- tools/yamllint.sh
+++ tools/yamllint.sh
@@ -2,19 +2,21 @@
 #
 # run yamllint in one command with parameters I prefer
 #
-# Requires Python package `yamllint`
+# Requires Python package `yamllint`, see `requirements.txt`
 #
 
 set -eu
 
 cd "$(dirname -- "${0}")/.."
 
+PYTHON=${PYTHON-$(which python 2>/dev/null || which python3 2>/dev/null)}
+
 set -x
 
-"${PYTHON-python3}" -m yamllint --version
+"${PYTHON}" -m yamllint --version
 
 exec \
-  "${PYTHON-python3}" -m yamllint \
+  "${PYTHON}" -m yamllint \
     --config-file ./tools/yamllint.yml \
     "./tools/yamllint.yml" \
     "./.github/workflows/rust.yml" \
