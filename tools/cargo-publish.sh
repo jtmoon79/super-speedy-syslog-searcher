aa4cf9d3fbc0b046ecfa814b298a27769783d209
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2024-03-22 10:06:39 -0700

    add tools/cargo-publish.sh

---
+++ tools/cargo-publish.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# run `cargo publish` with preferred options
+# pass --no-dry-run to actually publish
+#
+
+set -eu
+
+cd "$(dirname -- "${0}")/.."
+
+export RUST_BACKTRACE=1
+
+arg_dry_run='--dry-run'
+if [[ ${#} -eq 1 ]] && [[ "${1}" == "--no-dry-run" ]]; then
+    arg_dry_run=
+fi
+
+set -x
+
+cargo --version
+
+exec cargo publish \
+    --verbose \
+    --locked \
+    --all-features \
+    ${arg_dry_run} \
+    "${@}" \
