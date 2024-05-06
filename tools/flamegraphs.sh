9cb51c196060c66abf0430a85b32bdade4096484
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2024-05-05 22:59:51 -0700

    flamegraphs.sh bump FREQ, echo more

--- tools/flamegraphs.sh
+++ tools/flamegraphs.sh
@@ -10,34 +10,71 @@ SCRIPT="./tools/flamegraph.sh"
 
 DIROUT=${DIROUT-.}
 
-set -x
-
 export OUT="${DIROUT}/flamegraph-help.svg"
 FREQ=9999 "${SCRIPT}" '--help'
+echo
+echo
+
+export FREQ=7000
 
 export OUT="${DIROUT}/flamegraph-evtx.svg"
+(set -x
 "${SCRIPT}" './logs/programs/evtx/Microsoft-Windows-Kernel-PnP%4Configuration.evtx'
+)
+echo
+echo
 
 export OUT="${DIROUT}/flamegraph-journal.svg"
+(set -x
 "${SCRIPT}" './logs/programs/journal/Ubuntu22-user-1000x3.journal'
+)
+echo
+echo
 
 export OUT="${DIROUT}/flamegraph-syslog-no-matches.svg"
+(set -x
 "${SCRIPT}" './logs/other/tests/numbers3.log'
+)
+echo
 
 export OUT="${DIROUT}/flamegraph-syslog.svg"
+(set -x
 "${SCRIPT}" './logs/other/tests/gen-99999-1-Motley_Crue.log'
+)
+echo
+echo
 
 export OUT="${DIROUT}/flamegraph-syslog-gz.svg"
+(set -x
 "${SCRIPT}" './logs/other/tests/gen-1000-3-foobar.log.gz'
+)
+echo
+echo
 
 export OUT="${DIROUT}/flamegraph-syslog-lz4.svg"
+(set -x
 "${SCRIPT}" './logs/other/tests/gen-1000-3-foobar.log.lz4'
+)
+echo
+echo
 
 export OUT="${DIROUT}/flamegraph-syslog-xz.svg"
+(set -x
 "${SCRIPT}" './logs/other/tests/gen-1000-3-foobar.log.xz'
+)
+echo
+echo
 
 export OUT="${DIROUT}/flamegraph-syslog-tar.svg"
+(set -x
 "${SCRIPT}" './logs/other/tests/gen-1000-3-foobar.log.tar'
+)
+echo
+echo
 
 export OUT="${DIROUT}/flamegraph-utmp.svg"
+(set -x
 "${SCRIPT}" './logs/CentOS7/x86_64/wtmp'
+)
+echo
+echo
