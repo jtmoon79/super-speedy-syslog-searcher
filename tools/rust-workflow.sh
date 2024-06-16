59579ebed3d1b543f37e2a0f1c96ed8816eae23d
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2024-06-15 22:04:09 -0700

    (TOOLS) rust-workflow.sh tweak cross TARGETs

--- tools/rust-workflow.sh
+++ tools/rust-workflow.sh
@@ -38,33 +38,33 @@ cargo clippy --no-deps --all-targets
 cargo bench --no-run
 cargo build --profile flamegraph
 cargo build --profile valgrind
-for TARGET in (
-    aarch64-unknown-linux-gnu
-    i686-pc-windows-gnu
-    i686-pc-windows-msvc
-    i686-unknown-linux-gnu
-    x86_64-pc-windows-gnu
-    x86_64-pc-windows-msvc
-    x86_64-unknown-linux-gnu
-    aarch64-unknown-linux-musl
-    arm-unknown-linux-gnueabi
-    arm-unknown-linux-gnueabihf
-    armv7-unknown-linux-gnueabihf
-    powerpc-unknown-linux-gnu
-    powerpc64-unknown-linux-gnu
-    riscv64gc-unknown-linux-gnu
-    x86_64-unknown-freebsd
-    x86_64-unknown-illumos
-    x86_64-unknown-linux-musl
-    x86_64-unknown-netbsd
-    aarch64-linux-android
-    i686-linux-android
-    x86_64-pc-solaris
-    x86_64-sun-solaris
-    x86_64-linux-android
-    x86_64-unknown-redox
-    mips64-unknown-linux-gnuabi64
-); do
+for TARGET in \
+    aarch64-unknown-linux-gnu \
+    i686-pc-windows-gnu \
+    `# i686-pc-windows-msvc` \
+    i686-unknown-linux-gnu \
+    x86_64-pc-windows-gnu \
+    `# x86_64-pc-windows-msvc` \
+    x86_64-unknown-linux-gnu \
+    aarch64-unknown-linux-musl \
+    arm-unknown-linux-gnueabi \
+    arm-unknown-linux-gnueabihf \
+    armv7-unknown-linux-gnueabihf \
+    powerpc-unknown-linux-gnu \
+    powerpc64-unknown-linux-gnu \
+    riscv64gc-unknown-linux-gnu \
+    x86_64-unknown-freebsd \
+    x86_64-unknown-illumos \
+    x86_64-unknown-linux-musl \
+    x86_64-unknown-netbsd \
+    aarch64-linux-android \
+    i686-linux-android \
+    x86_64-pc-solaris \
+    x86_64-sun-solaris \
+    x86_64-linux-android \
+    x86_64-unknown-redox \
+    mips64-unknown-linux-gnuabi64 \
+; do
     cross check --lib --bins --target $TARGET
 done
 cargo doc --locked --release --frozen --no-deps
