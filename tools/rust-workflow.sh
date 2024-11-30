59386a5da238762470cf6db2d803ff885e2f072d
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2024-11-30 15:04:41 -0800

    (CI) rust.yml cross loongarch64-unknown-linux-gnu

--- tools/rust-workflow.sh
+++ tools/rust-workflow.sh
@@ -58,6 +58,7 @@ for TARGET in \
     x86_64-pc-windows-gnu \
     `# x86_64-pc-windows-msvc` \
     x86_64-unknown-linux-gnu \
+    loongarch64-unknown-linux-gnu \
     aarch64-unknown-linux-musl \
     arm-unknown-linux-gnueabi \
     arm-unknown-linux-gnueabihf \
