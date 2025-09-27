869f20baad04ae2bc8467fc20540ff69ee690880
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2025-09-27 11:38:51 -0700

    Revert "(CI) do not test x86_64-unknown-redox"
    This reverts commit 058026387c3b9ca4f072aa187754796421bbdfc9.

--- tools/rust-workflow.sh
+++ tools/rust-workflow.sh
@@ -81,7 +81,7 @@ for TARGET in \
     x86_64-pc-solaris \
     x86_64-sun-solaris \
     x86_64-linux-android \
-    `# x86_64-unknown-redox` \
+    x86_64-unknown-redox \
     `# mips64-unknown-linux-gnuabi64` \
 ; do
     cross check --lib --bins --target $TARGET
