9ad1616b91ffe3bfba3ab097dc04df90ba05845d
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2025-12-27 22:09:25 -0800

    (TOOLS) rust-gdb.sh add --silent, pass args as-is

--- tools/rust-gdb.sh
+++ tools/rust-gdb.sh
@@ -10,24 +10,26 @@ cd "$(dirname -- "${0}")/.."
 export RUST_BACKTRACE=1
 
 # allow user to pass -- to place extra arguments past the prescripted -- delimiter
-declare -a args1=()
-for a in "${@}"; do
-    if [[ "${a}" == "--" ]]; then
-        shift
-        break
-    fi
-    args1[${#args1[@]}]=${a}
-    shift
-done
-declare -a args2=()
-for a in "${@}"; do
-    args2[${#args2[@]}]=${a}
-done
+# declare -a args1=()
+# for a in "${@}"; do
+#     if [[ "${a}" == "--" ]]; then
+#         shift
+#         break
+#     fi
+#     args1[${#args1[@]}]=${a}
+#     shift
+# done
+# declare -a args2=()
+# for a in "${@}"; do
+#     args2[${#args2[@]}]=${a}
+# done
 
 target=$(ls -1tr target/debug/deps/s4-* | tail -n1)
 
+(set -x; rust-gdb --version)
+
+echo
+
 set -x
 
-rust-gdb --version
-
-exec rust-gdb "${target}" -ex 'layout split' "${@}"
+exec rust-gdb --silent "${target}" -ex 'layout split' "${@}"
