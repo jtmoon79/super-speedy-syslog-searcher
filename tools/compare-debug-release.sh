311b7f43f0d50acd79b5c39aec78b9b4bbd4bbbb
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2026-01-11 20:25:33 -0800

    (TOOLS) compare-debug-release.sh compare individually

--- tools/compare-debug-release.sh
+++ tools/compare-debug-release.sh
@@ -12,31 +12,44 @@ set -euo pipefail
 
 cd "$(dirname "${0}")/.."
 
-# output of the release run
-tmpr=$(mktemp -t "tmp.s4.compare-debug-release_release_XXXXX")
-# output of the debug run
-tmpd=$(mktemp -t "tmp.s4.compare-debug-release_debug_XXXXX")
+DIFF=diff
+if which colordiff &>/dev/null; then
+    DIFF=colordiff
+fi
+readonly DIFF
+
 # logs to process listed one per line
-declare -r logs=./tools/compare-debug-release_logs.txt
-readonly tmpr tmpd
+declare -r logs_file=./tools/compare-debug-release_logs.txt
 
 #
 # print some info for the script user, verify the s4 programs can run
 #
 
+declare -i ret=0
+declare -a logs=()
+while read log; do
+    if [[ "${log}" =~ ^#.*$ || -z "${log}" ]]; then
+        # skip comments and empty lines
+        continue
+    fi
+    logs+=("${log}")
+done <<< "$(cat "${logs_file}")"
 echo >&2
-cat "${logs}" >&2
-echo >&2
-echo "$(wc -l < "${logs}") files in \"${logs}\"" >&2
+echo "Comparing results of processing ${#logs[@]} files from \"${logs_file}\"" >&2
 echo >&2
 
-(set -x; diff --version) | head -n1
+(set -x; "${DIFF}" --version) | head -n1
+echo >&2
+
 PROGRAMR=${PROGRAMR-./target/release/s4}
 (set -x; "${PROGRAMR}" --version)
+readonly PROGRAMR
+
 PROGRAMD=${PROGRAMD-./target/debug/s4}
 (set -x; "${PROGRAMD}" --version 2>/dev/null)
 echo >&2
-readonly PROGRAMR PROGRAMD
+readonly PROGRAMD
+
 
 #
 # run s4 release and debug builds
@@ -56,26 +69,41 @@ declare -ar S4_ARGS=(
     --dt-after='19990303T000000+0000'
     --dt-before='20240210T221032+0000'
     --summary
-    '-'
     "${@}"
 )
 
-echo "${PS4}cat '${logs}'" >&2
+# output of the release run
+tmpr=$(mktemp -t "tmp.s4.compare-debug-release_release_XXXXX")
+# output of the debug run
+tmpd=$(mktemp -t "tmp.s4.compare-debug-release_debug_XXXXX")
+
+for log in "${logs[@]}"; do
+    echo "${log}" >&2
+    # run the release build
+    "${PROGRAMR}" "${S4_ARGS[@]}" "${log}" 2>/dev/null > "${tmpr}" || true
+    # run the debug build (might take a few minutes)
+    "${PROGRAMD}" "${S4_ARGS[@]}" "${log}" 2>/dev/null > "${tmpd}" || true
+
+    if ! "${DIFF}" --text --brief "${tmpr}" "${tmpd}"; then
+        echo -e "Files are not the same for log '${log}'.\n"
+        ret=1
+    fi
+done
+
+echo >&2
 
 # run the release build
 time (
     set -x
-    "${PROGRAMR}" "${S4_ARGS[@]}" 2>/dev/null > "${tmpr}" < "${logs}"
+    "${PROGRAMR}" "${S4_ARGS[@]}" "${logs[@]}" 2>/dev/null > "${tmpr}"
 ) || true
 
 echo >&2
 
-echo "${PS4}cat '${logs}'" >&2
-
 # run the debug build (might take a few minutes)
 time (
     set -x
-    "${PROGRAMD}" "${S4_ARGS[@]}" 2>/dev/null > "${tmpd}" < "${logs}"
+    "${PROGRAMD}" "${S4_ARGS[@]}" "${logs[@]}" 2>/dev/null > "${tmpd}"
 ) || true
 
 #
@@ -97,18 +125,17 @@ echo "super_speedy_syslog_searcher debug output '${tmpd}'"
 echo "  Line Count ${s4d_lc}"
 echo "  Byte Count ${s4d_bc}"
 
-DIFF=diff
-if which colordiff &>/dev/null; then
-    DIFF=colordiff
-fi
-
-declare -i ret=0
 if ! "${DIFF}" --text --brief "${tmpr}" "${tmpd}"; then
     ret=1
     echo "Files are not the same. (ಠ_ಠ)"
     echo
     echo "Difference Preview:"
-    ((set -x; "${DIFF}" --text -y --width=${COLUMNS-120} --suppress-common-lines "${tmpr}" "${tmpd}") || true) | head -n 20
+    (
+        (
+            set -x;
+            "${DIFF}" --text -y --width=${COLUMNS-120} --suppress-common-lines "${tmpr}" "${tmpd}"
+        ) || true
+    ) | head -n 20
     echo
 else
     echo
