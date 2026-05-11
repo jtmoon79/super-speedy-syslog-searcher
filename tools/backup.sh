0f93ff94e2b5aa133594cdfcac49abf4770c13f7
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2026-05-10 20:15:52 -0700

    (TOOLS) backup.sh use zip

--- tools/backup.sh
+++ tools/backup.sh
@@ -2,7 +2,7 @@
 #
 # backup.sh
 #
-# a quick manual backup script using 7zip
+# a quick manual backup script using zip
 #
 # set BACKUPDIR to the final backup destination, defaults to ".."
 #
@@ -23,63 +23,19 @@ if [[ ! -w "${BACKUPDIR}" ]]; then
     exit 1
 fi
 
-if ! Zz=$(which 7z); then
-    echo "7z not found in PATH" >&2
-    exit 1
-fi
-
 HERE="$(basename -- "$(realpath .)")"
 ZIPFILE="${BACKUPDIR}/${HERE}-$(date '+%Y%m%dT%H%M%S')-$(hostname).zip"
 
 (
 set -x
-
-# if this fails then `7z` is probably not the 7-zip this script expects
-"${Zz}" i &> /dev/null
-
-# backup the project!
-# ignore `target` directories in bindgen/ and the root
-"${Zz}" a -spf -ssc -bb1 -bt -stl -snl -scsUTF-8 -tzip -- "${ZIPFILE}" \
-    ./benches/ \
-    ./Cargo.toml \
-    ./Cargo.lock \
-    ./CHANGELOG.md \
-    ./deny.toml \
-    ./Extended-Thoughts.md \
-    ./.git/ \
-    ./.gitattributes \
-    ./.git_hooks/ \
-    ./.github/ \
-    ./.gitignore \
-    ./LICENSE.txt \
-    ./logs/ \
-    $(ls -d1 \
-        ./performance-data \
-        ./valgrind \
-        *.log \
-        ./Notes.txt \
-        ./*.svg \
-        ./tests \
-        ./trace.fxt.gz \
-        2>/dev/null || true
-    ) \
-    ./.mlc.toml \
-    ./README.md \
-    ./releases/ \
-    ./rustfmt.toml \
-    ./.taplo.toml \
-    ./src/ \
-    $(find ./subprojects/bindgen/ \
-        -mindepth 1 -maxdepth 1 \
-        -not -name 'target') \
-    ./tools/ \
-    ./.vscode/ \
-
-"${Zz}" l "${ZIPFILE}"
+zip --version
+find . \( -type d \( -name target -or -name .venv \) -prune \) -or -type f -print \
+    | sort \
+    | zip -9 "${ZIPFILE}" -o -@
 )
 
 # remove write permissions from the archive file
 # this may fail on NTFS or SMB mounts
-chmod -w -- "${ZIPFILE}" || true
+chmod -v -w -- "${ZIPFILE}" || true
 
 ls -lh "${ZIPFILE}"
