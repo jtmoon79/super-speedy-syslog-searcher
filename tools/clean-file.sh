03eea397b60ebb1f1ddaf8d3747db1517bd6b9b1
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2026-05-10 17:21:13 -0700

    (TOOLS) clean-file.sh be selective about sed expressions used

--- tools/clean-file.sh
+++ tools/clean-file.sh
@@ -4,14 +4,31 @@
 
 set -euo pipefail
 
+function re_escape () {
+    ${PYTHON-python3} -c 'import re; s = """ '"${1}"' """; se = re.escape(s[1:-1]); print(se)'
+}
+
 function clean_file () {
     # remove specific names from the passed file path $1
+    # carefully add exprssions for `sed`
+
+    declare -a args=(
+        -e "s|$(hostname)|host|g"
+    )
+    declare -r rp=$(re_escape "$(realpath .)")
+    if [[ "$rp" ]]; then
+        args+=(-e "s|${rp}|.|g")
+    fi
+    declare -r home=$(re_escape "${HOME}")
+    if [[ "$home" ]]; then
+        args+=(-e "s|${home}|/home|g")
+    fi
+    if [[ "$USER" ]]; then
+        args+=(-e "s|${USER}|user|g")
+    fi
     set -x
     exec sed -i \
-        -e "s|$(realpath .)|.|g" \
-        -e "s|${HOME}|/home|g" \
-        -e "s|$(hostname)|host|g" \
-        -e "s|${USER}|user|g" \
+        "${args[@]}" \
         -- \
         "${1}"
 }
