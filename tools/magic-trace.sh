bbfa880ac2f188abb21da4e77253a65be8fb3f07
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2026-05-10 17:19:53 -0700

    (TOOLS) magic-trace.sh find perf

--- tools/magic-trace.sh
+++ tools/magic-trace.sh
@@ -27,6 +27,42 @@ fi
     "${MAGIC_TRACE}" version
 )
 
+# make a best-effort attempt to find the `perf` program which may reside
+# at an unusual path not in the environment PATH.
+# the perf installed to `/usr/bin/perf` is often a stub.
+# print the full path of perf.
+# TODO: move `perf_path` to a sourced `common.sh`
+function perf_path() {
+    local perf_path_candidate
+    for perf_path_candidate in \
+        "/usr/lib/linux-tools/$(ls -1v /usr/lib/linux-tools/ 2>/dev/null | tail -n1 || true)/perf" \
+        "/usr/lib/linux-tools-$(uname -r)/perf" \
+        "$(find /usr/lib/ -name perf -type f 2>/dev/null | sort | head -n1 || true)" \
+        "/usr/lib64/perf" \
+        "/usr/lib/perf" \
+        "/usr/lib/linux-tools/$(uname -r)/perf" \
+        "$(which perf 2>/dev/null)"
+    do
+        if [[ -e "${perf_path_candidate}" ]]; then
+            echo -n "${perf_path_candidate}"
+            return 0
+        fi
+    done
+
+    return 1
+}
+
+if [[ ! "${PERF+x}" ]]; then
+    PERF=${PERF-$(perf_path)} || true
+    if [[ ! -e "${PERF}" ]]; then
+        echo "ERROR: PERF tool not found at '${PERF}'" >&2
+        exit 1
+    fi
+fi
+export PERF
+echo "using perf '$PERF'" >&2
+export PATH=${PATH}:$(dirname "$PERF")
+
 S4_PROGRAM=${S4_PROGRAM-"$(dirname -- "${0}")/../target/flamegraph/s4"}
 S4_DIR=$(dirname "${S4_PROGRAM}")
 
