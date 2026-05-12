7fe0d4ea7e3afa19f48fe1bac366879105ca3137
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2026-05-11 20:47:08 -0700

    (PYTHON) pip-tools-compile.sh use $PYTHON

--- src/python/tools/pip-tools-compile.sh
+++ src/python/tools/pip-tools-compile.sh
@@ -5,6 +5,8 @@
 # install `requirements-dev.in` prior to have piptools available
 #
 
+PYTHON=${PYTHON:-python3}
+
 # must match pyprojec.toml:requires-python
 readonly PYTHON_VERSION_REQUIRED="3.9"
 
@@ -12,7 +14,7 @@ set -eu
 
 function version_major_minor_eq() {
     # return 0 if major.minor parts of two versions are equal, 1 otherwise
-    python -c "
+    "${PYTHON}" -c "
 import sys
 from packaging.version import Version
 
@@ -24,22 +26,22 @@ sys.exit(0 if result else 1)
 }
 
 function print_python_version() {
-    python -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}")'
+    "${PYTHON}" -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}")'
 }
 
 version=$(print_python_version)
 
 if ! version_major_minor_eq "$version" "$PYTHON_VERSION_REQUIRED"; then
-    echo "ERROR: this script must be run with Python $PYTHON_VERSION_REQUIRED (current: $version)" >&2
+    echo "ERROR: this script must be run with python $PYTHON_VERSION_REQUIRED (current: $version)" >&2
+    echo "       Hint: override PYTHON to a 3.9 binary" >&2
     exit 1
 fi
 
-cd "$(dirname "$0")/../s4_event_readers"
+DIR_=$(realpath "$(dirname "$0")/../s4_event_readers")
 
 for outfile_infile in \
-    "requirements.txt|requirements.in" \
-    `#"requirements-3.9.txt|requirements-3.9.in"` \
-    "requirements-dev.txt|requirements-dev.in"
+    "${DIR_}/requirements.txt|${DIR_}/requirements.in" \
+    "${DIR_}/requirements-dev.txt|${DIR_}/requirements-dev.in"
 do
     outfile=$(echo -n "$outfile_infile" | cut -d'|' -f1)
     rm -f "$outfile"
@@ -48,7 +50,7 @@ do
         set -x
         # passing `--generate-hashes` here causes pip-compile to fail
         # see https://github.com/jazzband/pip-tools/issues/2299
-        python -m piptools compile \
+        "${PYTHON}" -m piptools compile \
             --strip-extras \
             --annotate \
             --emit-find-links \
