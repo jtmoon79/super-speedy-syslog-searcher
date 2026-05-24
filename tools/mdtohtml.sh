440cbf9d744f288a78dfec48479bb7e3ef3ba4eb
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2026-05-23 18:55:34 -0700

    (TOOLS) mdtothtml.sh apply simple css

--- tools/mdtohtml.sh
+++ tools/mdtohtml.sh
@@ -15,7 +15,29 @@ file=${1}
 file_html="${file}.html"
 shift
 
+css_file=$(mktemp --suffix=.css)
+trap 'rm -f -- "${css_file}"' EXIT
+echo '
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th,
+td {
+  border: 1px solid #666;
+  padding: 0.5rem 0.75rem;
+  text-align: left;
+}
+
+th {
+  background: #e9ecef;
+  font-weight: 700;
+  border-bottom: 2px solid #333;
+}
+' > "${css_file}"
+
 set -x
 
 which mdtohtml
-exec mdtohtml -page "$file" "${@}" > "${file_html}"
+exec mdtohtml  -css "${css_file}" -page "$file" "${@}" > "${file_html}"
