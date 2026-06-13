bfac49f810578b6afb3c84fb88656077d018f7c5
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2026-06-12 23:55:22 -0700

    (TOOLS) performance-plot.sh fix FILE_NUM_MAX

--- tools/performance-plots.sh
+++ tools/performance-plots.sh
@@ -24,9 +24,6 @@ else
         ./logs/NetBSD9.3/Xorg.0.log
         ./logs/OpenBSD7.4/x86_64/utmp
         ./logs/other/tests/dtf5-6a.log
-        # TODO: [2026/05] log file gen-99999-1-Motley_Crue.log tends to stall around 125 FILE_NUM
-        #       probably worth understanding why
-        "./logs/other/tests/gen-99999-1-Motley_Crue.log${SEP}100"
         ./logs/other/tests/gen-1000-3-foobar-noyear.log
         ./logs/other/tests/gen-1000-3-foobar.log
         ./logs/other/tests/gen-1000-3-foobar.log.bz2
@@ -38,6 +35,9 @@ else
         ./logs/other/tests/gen-1000-3-foobar.UTF-16LE.log
         ./logs/other/tests/gen-1000-3-foobar.UTF-16LE_BOM.log
         ./logs/other/tests/gen-1000-3-foobar.UTF-32LE.log
+        # TODO: [2026/05] log file gen-99999-1-Motley_Crue.log tends to stall around 125 FILE_NUM
+        #       probably worth understanding why
+        "./logs/other/tests/gen-99999-1-Motley_Crue.log${SEP}100"
         ./logs/programs/evtx/Microsoft-Windows-Kernel-PnP__Configuration.evtx
         ./logs/programs/journal/RHE_91_system.journal
         ./logs/programs/OneDrive/Local/Microsoft/OneDrive/logs/Common/FileCoAuth-2025-12-21.1216.11020.2.odl
@@ -50,6 +50,8 @@ declare -i FILE_NUM=${FILE_NUM:-300}
 for FILE in "${FILES[@]}"; do
     declare -i START_TIME=${SECONDS}
     # check for optional `|FILE_NUM_MAX`
+    unset FILE_NUM_MAX
+    FILE_NUM_MAX=
     if [[ "${FILE}" == *"${SEP}"* ]]; then
         FILE_=$(echo -n "${FILE}" | cut -d "${SEP}" -f1)
         declare -i FILE_NUM_MAX=$(echo -n "${FILE}" | cut -d "${SEP}" -f2)
@@ -57,12 +59,12 @@ for FILE in "${FILES[@]}"; do
             echo "Invalid FILE_NUM_MAX ${FILE_NUM_MAX} for string '${FILE}'" >&2
             exit 1
         fi
-        if [[ "${FILE_NUM}" -gt "${FILE_NUM_MAX}" ]]; then
-            FILE_NUM=${FILE_NUM_MAX}
-        fi
         FILE=${FILE_}
     fi
     (
+        if [[ "${FILE_NUM_MAX-}" ]] && [[ "${FILE_NUM}" -gt "${FILE_NUM_MAX}" ]]; then
+            FILE_NUM=${FILE_NUM_MAX}
+        fi
         set -x
         env FILE="${FILE}" FILE_NUM="${FILE_NUM}" ./tools/performance-plot.sh "${@}"
     )
