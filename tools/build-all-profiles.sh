bbb1418a64882b32d9c0020f666ffa9764d86b68
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2026-06-17 17:04:37 -0700

    (TOOLS) build-all-profiles.sh only build 2 at time; avoid OOMs

--- tools/build-all-profiles.sh
+++ tools/build-all-profiles.sh
@@ -6,9 +6,12 @@ set -euo pipefail
 
 cd "$(dirname -- "${0}")/.."
 
-# debug, release, jemalloc
+export S4_BUILD_PRINT=1
+export S4_BUILD_REGEX_PRINT=1
 
 set -x
+
+# debug, release
 
 declare -a PIDs=()
 
@@ -16,16 +19,24 @@ cargo build --locked &
 PIDs+=($!)
 cargo build --locked --profile release &
 PIDs+=($!)
-cargo build --locked --profile jemalloc --features jemalloc &
-PIDs+=($!)
 
 wait ${PIDs[@]}
 
 ./target/debug/s4 --version
 ./target/release/s4 --version
+
+# jemalloc
+
+PIDs=()
+
+cargo build --locked --profile jemalloc --features jemalloc &
+PIDs+=($!)
+
+wait ${PIDs[@]}
+
 ./target/jemalloc/s4 --version
 
-# mimalloc, tcmalloc, rpmalloc
+# mimalloc, tcmalloc
 
 PIDs=()
 
@@ -33,16 +44,24 @@ cargo build --locked --profile mimalloc --features mimalloc &
 PIDs+=($!)
 cargo build --locked --profile tcmalloc --features tcmalloc &
 PIDs+=($!)
-cargo build --locked --profile rpmalloc --features rpmalloc &
-PIDs+=($!)
 
 wait ${PIDs[@]}
 
 ./target/mimalloc/s4 --version
 ./target/tcmalloc/s4 --version
+
+# rpmalloc
+
+PIDs=()
+
+cargo build --locked --profile rpmalloc --features rpmalloc &
+PIDs+=($!)
+
+wait ${PIDs[@]}
+
 ./target/rpmalloc/s4 --version
 
-# alloc_tracker, flamegraph, valgrind
+# alloc_tracker, flamegraph
 
 PIDs=()
 
@@ -50,11 +69,19 @@ cargo build  --locked --profile alloc_tracker --features alloc_tracker &
 PIDs+=($!)
 RUSTFLAGS=-g cargo build --locked --profile flamegraph &
 PIDs+=($!)
-RUSTFLAGS=-g cargo build --locked --profile valgrind &
-PIDs+=($!)
 
 wait ${PIDs[@]}
 
 ./target/alloc_tracker/s4 --version
 ./target/flamegraph/s4 --version
+
+# valgrind
+
+PIDs=()
+
+RUSTFLAGS=-g cargo build --locked --profile valgrind &
+PIDs+=($!)
+
+wait ${PIDs[@]}
+
 ./target/valgrind/s4 --version
