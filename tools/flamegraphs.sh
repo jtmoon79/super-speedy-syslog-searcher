329a2a17b6d77199a2c952f073baeaa419fbef14
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2026-06-19 22:14:35 -0700

    (TOOLS) flamegraphs.sh rename .svg

--- tools/flamegraphs.sh
+++ tools/flamegraphs.sh
@@ -18,26 +18,26 @@ echo
 export FREQ=8000
 
 for FLAMGRAPHOUT_S4FILE in \
-    "flamegraph-evtx.svg|./logs/programs/evtx/Microsoft-Windows-Kernel-PnP__Configuration.evtx" \
-    "flamegraph-asl.svg|./logs/MacOS11/DiagnosticMessages/2023.10.31.asl" \
-    "flamegraph-etl.svg|./logs/programs/Event_Trace_Log/WindowsUpdate.20251008.140245.443.8.etl" \
-    "flamegraph-odl.svg|./logs/programs/OneDrive/Local/Microsoft/OneDrive/logs/Common/FileCoAuth-2025-12-21.1216.11020.2.odl" \
-    "flamegraph-journal.svg|./logs/programs/journal/RHE_91_system.journal" \
-    "flamegraph-journal-bz2.svg|./logs/programs/journal/RHE_91_system.journal.bz2" \
-    "flamegraph-journal-gz.svg|./logs/programs/journal/RHE_91_system.journal.gz" \
-    "flamegraph-journal-lz4.svg|./logs/programs/journal/RHE_91_system.journal.lz4" \
-    "flamegraph-journal-xz.svg|./logs/programs/journal/RHE_91_system.journal.xz" \
-    "flamegraph-journal-tar.svg|./logs/programs/journal/RHE_91_system.tar" \
+    "flamegraph-evtx__Microsoft-Windows-Kernel-PnP__Configuration.evtx.svg|./logs/programs/evtx/Microsoft-Windows-Kernel-PnP__Configuration.evtx" \
+    "flamegraph-asl__2023.10.31.asl.svg|./logs/MacOS11/DiagnosticMessages/2023.10.31.asl" \
+    "flamegraph-etl__WindowsUpdate.20251008.140245.443.8.etl.svg|./logs/programs/Event_Trace_Log/WindowsUpdate.20251008.140245.443.8.etl" \
+    "flamegraph-odl__FileCoAuth-2025-12-21.1216.11020.2.odl.svg|./logs/programs/OneDrive/Local/Microsoft/OneDrive/logs/Common/FileCoAuth-2025-12-21.1216.11020.2.odl" \
+    "flamegraph-journal__RHE_91_system.journal.svg|./logs/programs/journal/RHE_91_system.journal" \
+    "flamegraph-journal-bz2__RHE_91_system.journal.bz2.svg|./logs/programs/journal/RHE_91_system.journal.bz2" \
+    "flamegraph-journal-gz__RHE_91_system.journal.gz.svg|./logs/programs/journal/RHE_91_system.journal.gz" \
+    "flamegraph-journal-lz4__RHE_91_system.journal.lz4.svg|./logs/programs/journal/RHE_91_system.journal.lz4" \
+    "flamegraph-journal-xz__RHE_91_system.journal.xz.svg|./logs/programs/journal/RHE_91_system.journal.xz" \
+    "flamegraph-journal-tar__RHE_91_system.tar.svg|./logs/programs/journal/RHE_91_system.tar" \
     "flamegraph-syslog-empty.svg|./logs/other/tests/empty.log" \
-    "flamegraph-syslog-no-matches.svg|./logs/other/tests/numbers3.log" \
-    "flamegraph-syslog.svg|./logs/other/tests/gen-99999-1-Motley_Crue.log" \
-    "flamegraph-syslog-bz2.svg|./logs/other/tests/gen-1000-3-foobar.log.bz2" \
-    "flamegraph-syslog-gz.svg|./logs/other/tests/gen-1000-3-foobar.log.gz" \
-    "flamegraph-syslog-lz4.svg|./logs/other/tests/gen-1000-3-foobar.log.lz4" \
-    "flamegraph-syslog-xz.svg|./logs/other/tests/gen-1000-3-foobar.log.xz" \
-    "flamegraph-syslog-tar.svg|./logs/other/tests/gen-1000-3-foobar.log.tar" \
-    "flamegraph-syslog-noyear.svg|./logs/other/tests/gen-1000-3-foobar-noyear.log" \
-    "flamegraph-utmp.svg|./logs/CentOS7/x86_64/wtmp" \
+    "flamegraph-syslog-no-matches__numbers3.log.svg|./logs/other/tests/numbers3.log" \
+    "flamegraph-syslog__gen-99999-1-Motley_Crue.log.svg|./logs/other/tests/gen-99999-1-Motley_Crue.log" \
+    "flamegraph-syslog-bz2__gen-1000-3-foobar.log.bz2.svg|./logs/other/tests/gen-1000-3-foobar.log.bz2" \
+    "flamegraph-syslog-gz__gen-1000-3-foobar.log.gz.svg|./logs/other/tests/gen-1000-3-foobar.log.gz" \
+    "flamegraph-syslog-lz4__gen-1000-3-foobar.log.lz4.svg|./logs/other/tests/gen-1000-3-foobar.log.lz4" \
+    "flamegraph-syslog-xz__gen-1000-3-foobar.log.xz.svg|./logs/other/tests/gen-1000-3-foobar.log.xz" \
+    "flamegraph-syslog-tar__gen-1000-3-foobar.log.tar.svg|./logs/other/tests/gen-1000-3-foobar.log.tar" \
+    "flamegraph-syslog-noyear__gen-1000-3-foobar-noyear.log.svg|./logs/other/tests/gen-1000-3-foobar-noyear.log" \
+    "flamegraph-utmp__CentOS7_x86_64_wtmp.svg|./logs/CentOS7/x86_64/wtmp" \
 ; do
     (
         export OUT="${DIROUT}/${FLAMGRAPHOUT_S4FILE%%|*}"
