77c38995ac688cfdafa118665d516e90fc024dea
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2026-07-26 11:15:12 -0700

    (LIB) add regex 45
    Issue #506

--- src/tests/common.rs
+++ src/tests/common.rs
@@ -792,7 +792,7 @@ lazy_static! {
     pub static ref FILE_UTF8_BOM_DTF56B_FPATH: FPath = path_to_fpath(&FILE_UTF8_BOM_DTF56B_PATH);
 }
 
-pub const REGEX_ID_DTF56B: RegexId = 79;
+pub const REGEX_ID_DTF56B: RegexId = 46;
 
 // ---------
 
